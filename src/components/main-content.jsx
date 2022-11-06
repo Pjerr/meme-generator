934bd8cb7ddfe55eb678057d02fc0cd8b95e5a56
Petar <petar.milisavljevic@elfak.rs>
2022-11-06 11:28:02 +0100

    added mock data

--- src/components/main-content.jsx
+++ src/components/main-content.jsx
@@ -1,6 +1,23 @@
 import React from "react";
-
+import memesData from "../memesData";
 export class MainContent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentMeme: null,
+    };
+    this.getMemeImage = this.getMemeImage.bind(this);
+  }
+
+  getMemeImage() {
+    const memesArrray = memesData.data.memes;
+    const i = Math.floor(Math.random() * memesArrray.length);
+    const meme = memesArrray[i];
+    this.setState({ currentMeme: meme }, () => {
+      console.log(this.state.currentMeme);
+    });
+  }
+
   render() {
     return (
       <div className="main-form">
@@ -8,10 +25,20 @@ export class MainContent extends React.Component {
           <input type="text" placeholder="Top text" />
           <input type="text" placeholder="Bottom text" />
         </div>
-        <button type="button" className="form-button">
+        <button
+          type="button"
+          className="form-button"
+          onClick={this.getMemeImage}
+        >
           Generate meme image
         </button>
-        <img src="" alt="slika" className="meme-image"></img>
+        {this.state.currentMeme !== null && (
+          <img
+            src={this.state.currentMeme.url}
+            alt="slika"
+            className="meme-image"
+          ></img>
+        )}
       </div>
     );
   }
